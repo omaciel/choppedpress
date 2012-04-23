c6f8622cc832ab109e7076d20e30804c88cf7ac2
Og B. Maciel <ogmaciel@gnome.org>
2012-04-23 15:16:26 -0400

    Changed README.md to README.rst as markdown was giving me issues when registering with PyPi.

--- setup.py
+++ setup.py
@@ -1,23 +1,17 @@
 # -*- encoding: utf-8 -*-
 # vim: ts=4 sw=4 expandtab ai
 
-with open('README.md') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
-
 from setuptools import setup, find_packages
 
 setup(
         name = "choppedpress",
         version = "0.0.1",
         description = "Split WordPress XML export files into smaller files to import into a new WordPress installations.",
-        long_description = readme,
+        long_description = open("README.rst", 'r').read(),
         author = "Og Maciel",
         author_email = "omaciel@ogmaciel.com",
         url="http://omaciel.github.com/choppedpress/",
-        license = license,
+        license = open("LICENSE", 'r').read(),
         packages = find_packages(exclude=('tests', 'docs',)),
         scripts = ['choppedpress',]
         )
