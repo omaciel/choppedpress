127da47e9d8521033acf3e12db477d7d532254b9
Og B. Maciel <ogmaciel@gnome.org>
2012-04-23 14:05:26 -0400

    More metadata addition to setup.py.

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ from setuptools import setup, find_packages
 setup(
         name = "choppedpress",
         version = "0.0.1",
-        description = "",
+        description = "Split WordPress XML export files into smaller files to import into a new WordPress installations.",
         long_description = readme,
         author = "Og Maciel",
         author_email = "omaciel@ogmaciel.com",
         url="http://omaciel.github.com/choppedpress/",
         license = license,
-        packages = find_packages(),
+        packages = find_packages(exclude=('tests', 'docs',)),
         scripts = ['choppedpress',]
         )
